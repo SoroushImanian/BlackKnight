f75ce18943546b9eaca94979546112604b734744
Soroush Imanian <soroushimanian@gmail.com>
2025-08-18 03:52:58 +0330

    Bug fixed

--- bot.php
+++ bot.php
@@ -5,14 +5,14 @@ date_default_timezone_set('Asia/Tehran');
 define('MAX_MIX_CONFIGS', 500);
 
 $protocols = [
-    'vless'     => '/vless:\/\/[^<>\'\"\s]+/',
-    'vmess'     => '/vmess:\/\/[^<>\'\"\s]+/',
-    'ss'        => '/ss:\/\/[^<>\'\"\s]+/',
-    'trojan'    => '/trojan:\/\/[^<>\'\"\s]+/',
-    'hysteria'  => '/hy2:\/\/[^<>\'\"\s]+/',
-    'tuic'      => '/tuic:\/\/[^<>\'\"\s]+/',
-    'anytls'    => '/anytls:\/\/[^<>\'\"\s]+/',
-    'wireguard' => '/wireguard:\/\/[^<>\'\"\s]+/',
+    'vless'     => '/vless:\/\/\S+/',
+    'vmess'     => '/vmess:\/\/\S+/',
+    'ss'        => '/ss:\/\/\S+/',
+    'trojan'    => '/trojan:\/\/\S+/',
+    'hysteria'  => '/hy2:\/\/\S+/',
+    'tuic'      => '/tuic:\/\/\S+/',
+    'anytls'    => '/anytls:\/\/\S+/',
+    'wireguard' => '/wireguard:\/\/\S+/',
 ];
 
 function fetchContent($url) {
@@ -37,12 +37,13 @@ $allConfigs = array_fill_keys(array_keys($protocols), []);
 
 foreach ($telegramChannelURLs as $channelURL) {
     try {
-        $content = fetchContent($channelURL);
+        $rawContent = fetchContent($channelURL);
         
-        $decodedContent = html_entity_decode($content);
-        
+        $plainText = strip_tags($rawContent);
+        $cleanedContent = html_entity_decode($plainText);
+
         foreach ($protocols as $name => $pattern) {
-            if (preg_match_all($pattern, $decodedContent, $matches)) {
+            if (preg_match_all($pattern, $cleanedContent, $matches)) {
                 $allConfigs[$name] = array_merge($allConfigs[$name], $matches[0]);
             }
         }
@@ -74,7 +75,7 @@ foreach ($fileContents as $key => $content) {
     $finalContent = empty($content) ? '// Nothing yet' : preg_replace("/\n\s*\n/", "\n", $content);
 
     file_put_contents("sub/{$key}", $finalContent);
-    file_put_contents("sub/{$key}base64", base64_encode($finalContent));
+    file_put_contents("sub/{$key}base64", base_encode($finalContent));
 }
 
 echo "Config collection finished successfully. Total unique configs in mix: " . count($mixConfigs) . PHP_EOL;
